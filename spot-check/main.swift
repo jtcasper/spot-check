ca26e463b2a60ba6d6c33a84013bd6d9d75b1137
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-01-02 19:22:36 -0600

    Run app from main.swift
    Remove unnecessary views as app has no UI

---
+++ spot-check/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  spot-check
+//
+//  Created by Jacob Casper on 1/2/20.
+//  Copyright © 2020 ghostware. All rights reserved.
+//
+
+import Cocoa
+
+let delegate = AppDelegate()
+
+NSApplication.shared.delegate = delegate
+
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
