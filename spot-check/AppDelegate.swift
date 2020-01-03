c71ce71a785107b421fcf18a05e0abaf8df1b808
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-01-02 19:17:34 -0600

    Implement menu bar logic

--- spot-check/AppDelegate.swift
+++ spot-check/AppDelegate.swift
@@ -11,16 +11,40 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var statusItem: NSStatusItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        let menu = NSMenu.init()
+        menu.addItem(withTitle: "Quit", action: #selector(quit), keyEquivalent: "q")
+        self.statusItem.menu = menu
+
+
+        if let button = self.statusItem.button {
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+                button.title = self.getTrackTitle()
+            }
+        }
+
+    }
+
+    func getTrackTitle() -> String {
+        let script = (NSAppleScript.init(source: "tell application \"Spotify\" to (get name of current track) & \" - \" & (get artist of current track)"))!
+        var error = NSDictionary?(nil)
+        let info = script.executeAndReturnError(&error).stringValue
+        return info ?? "nil"
+    }
+
+
+    // MARK: - Exit
+
+    @objc func quit() {
+        NSApplication.shared.terminate(self)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
 }
-
