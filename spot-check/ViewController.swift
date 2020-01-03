4b319cfc350de7eb1dace053dafdbf98f7fdf8d3
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-01-02 18:34:10 -0600

    Initial Commit

---
+++ spot-check/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  spot-check
+//
+//  Created by Jacob Casper on 1/2/20.
+//  Copyright © 2020 ghostware. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
